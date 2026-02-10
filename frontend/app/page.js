f5bc5d96503b7bf3a2fa7694955cbda7e6c48a02
uzwal01 <uzwalduwal07@gmail.com>
2026-02-10 23:02:43 +0545

    applying cors

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sales`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/leaderboard`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
