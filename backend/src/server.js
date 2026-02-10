a205e20e39b67fb7638a16efdec323bc6b123c4f
uzwal01 <uzwalduwal07@gmail.com>
2026-02-10 22:53:01 +0545

    applying cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());    // Allow frontend to access backend
+app.use(cors({
+  origin: "https://sales-leaderboard-pi.vercel.app/"
+}));    // Allow frontend to access backend
 app.use(express.json());    // Parse JSON request bodies
 app.use(express.urlencoded({ extended: true }));    // Parse form data
 
